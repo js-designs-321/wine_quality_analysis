bd6effbfc5a1207bf7f33801f2b44d1ce69acf71
js-designs-321 <jatni.321@gmail.com>
2023-04-03 02:52:45 +0530

    Add home, predict and result pages

--- web/app.js
+++ web/app.js
@@ -10,20 +10,30 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-app.get("/",async function(req,res){
+app.get("/",function(req,res){
+    return res.render("home");
+})
 
+app.get('/predict',function(req,res){
+    return res.render("predict"); 
+})
+
+app.post('/result',function(req,res){
     const pythonProcess = spawn('/opt/homebrew/bin/python3', ['./load_model.py']);
+    let output = null; 
     pythonProcess.stdout.on('data', (data) => {
         console.log(`${data}`);
         // console.log(data.toJSON());
+        output = data.toJSON().data; 
     });
-    
-    // const userInput = '-0.33399129,0.24258119,-0.96313679,0.02140418,0.83730066,-0.16962178,0.00884052,0.23074469,-0.44256111,0.0634928,-0.75508835'
-    const userInput = '1.21575596,-0.03867236,1.9523043,-0.05555466,0.64485725,-0.8951552,-0.93924554,0.33053966,-1.40508642,0.0634928,1.21665571';
+    // const userInput = '-0.33399129,0.24258119,-0.96313679,0.02140418,0.83730066,-0.16962178,0.00884052,0.23074469,-0.44256111,0.0634928,-0.75508835'; 
+    // const userInput = '1.21575596,-0.03867236,1.9523043,-0.05555466,0.64485725,-0.8951552,-0.93924554,0.33053966,-1.40508642,0.0634928,1.21665571';
+    const userInput = req.body.fixedAcidity + "," + req.body.volatileAcidity + "," + req.body.citricAcid + "," + req.body.residualSugar + "," + req.body.chlorides + "," + req.body.freeSulphurDioxide + "," + req.body.totalSulphurDioxide + "," + req.body.density + "," + req.body.pH + "," + req.body.sulphates + "," + req.body.alcohol; 
+    console.log(userInput);
     pythonProcess.stdin.write(`${userInput}\n`);
-
-    res.render("home");
-    
+    return res.render("result",{
+        output : output
+    }); 
 })
 
 
